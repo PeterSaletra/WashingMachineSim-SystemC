77ab4b9e813dee16f4fea48e68f55ec689a87c78
Asasinxd <peter.saletra@gmail.com>
2024-10-24 11:04:08 +0000

    Started basic config

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,23 +1,42 @@
-// Learn with Examples, 2020, MIT license
-#include <systemc> // include the systemC header file
-using namespace sc_core; // use namespace
+#include <systemc>
+using namespace sc_core;
 
-void hello1() { // a normal c++ function
-  std::cout << "Hello world using approach 1" << std::endl;
-}
-
-struct HelloWorld : sc_module { // define a systemC module
-  SC_CTOR(HelloWorld) {// constructor function, to be explained later
-    SC_METHOD(hello2); // register a member function to the kernel
+SC_MODULE(PROC1){
+  SC_CTOR(PROC1){
+    SC_THREAD(run);
   }
-  void hello2(void) { // a function for systemC simulation kernel, void inside () can be omitted
-    std::cout << "Hello world using approach 2" << std::endl;
+
+  void run(){
+    while (true)
+    {
+      std::cout << "Proc 1" << std::endl;
+      wait(2, SC_SEC);
+    }
+  }
+
+};
+
+SC_MODULE(PROC2){
+  SC_CTOR(PROC2){
+    SC_THREAD(run);
+  }
+
+  void run(){
+    while (true)
+    {
+      std::cout << "Proc 2" << std::endl;
+      wait(2, SC_SEC);
+    }
   }
 };
 
 int sc_main(int, char*[]) { // entry point
-  hello1(); // approach #1: manually invoke a normal function
-  HelloWorld helloworld("helloworld"); // approach #2, instantiate a systemC module
-  sc_start(); // let systemC simulation kernel to invoke helloworld.hello2();
+
+  PROC2 proc2("PROC2");
+  PROC1 proc1("PROC1");
+  
+  sc_start();
+
   return 0;
 }
+
